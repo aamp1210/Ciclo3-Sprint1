1d9fe7a4e3a1afce079357cfaaff0fb7edd20e8e
Sebastian Franco <james.franco.velandia@unillanos.edu.co>
2021-09-29 20:13:33 -0500

    agregar js (commit on 'master')

---
+++ js/validaciones.js
@@ -0,0 +1,15 @@
+function validar_nombre_usuario(string){
+
+}
+
+function validar_anoNacimiento_usuario(){
+
+}
+
+function validar_contrasena(string){
+
+}
+
+module.exports.validar_nombre_usuario = validar_nombre_usuario;
+module.exports.validar_anoNacimiento_usuario = validar_anoNacimiento_usuario;
+module.exports.validar_contrasena = validar_contrasena;
