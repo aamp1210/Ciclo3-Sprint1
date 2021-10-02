4cac1bb4e8564601cb99d40026a2177c5e4d06d2
Sebastian Franco <james.franco.velandia@unillanos.edu.co>
2021-10-02 16:04:54 -0500

    Cambios finales Sprint 2 - Js y formulario

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,13 +1,18 @@
 function validar_nombre_usuario(nombre){
-
+    var exp = /[A-Z]([a-z]+)?/; //Expresion regular que cumple los requisitos
+    return exp.test(nombre);
 }
 
-function validar_anoNacimiento_usuario(){
-
+function validar_anoNacimiento_usuario(anio){
+    var exp = /(19[0-9][0-9]|20[0-2][0-1])/; //Expresion regular con los requisitos
+    return exp.test(anio);
 }
 
 function validar_contrasena(contrasena){
-
+    var exp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*)[A-Za-z\d$@$!%*?&]{6,20}$/;
+    return exp.test(contrasena);
 }
 
-console.log(document.getElementById('nombre_usuario'));
+module.exports.validar_nombre_usuario = validar_nombre_usuario;
+module.exports.validar_contrasena = validar_contrasena;
+module.exports.validar_anoNacimiento_usuario = validar_anoNacimiento_usuario;
