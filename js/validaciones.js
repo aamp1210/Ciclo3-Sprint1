c90b96c75d1b2e7a5b9b35643dc8c8bddf6c0633
Sebastian Franco <james.franco.velandia@unillanos.edu.co>
2021-10-06 21:59:06 -0500

    Ultimos ajustes sprint 2

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,15 +1,15 @@
 function validar_nombre_usuario(nombre){
-    var exp = /[A-Z]([a-z]+)?/; //Expresion regular que cumple los requisitos
+    var exp = /[A-Z]([a-z]+)?/; //Expresion solo letras la primera en mayuscula
     return exp.test(nombre);
 }
 
 function validar_anoNacimiento_usuario(anio){
-    var exp = /(19[0-9][0-9]|20[0-2][0-1])/; //Expresion regular con los requisitos
+    var exp = /(19[0-9][0-9]|20[0-2][0-1])/; //Expresion rango 1900 hasta 2021
     return exp.test(anio);
 }
 
 function validar_contrasena(contrasena){
-    var exp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*)[A-Za-z\d$@$!%*?&]{6,20}$/;
+    var exp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*)[A-Za-z\d$@$!%*?&]{6,20}$/; //Expresion contraseña
     return exp.test(contrasena);
 }
 
