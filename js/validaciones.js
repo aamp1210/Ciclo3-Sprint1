ced9f0c0da2a2149d6f1f47088c2fdb373cf8f72
Sebastian Franco <james.franco.velandia@unillanos.edu.co>
2021-10-07 12:44:37 -0500

    Ajustes de las validaciones del sprint 3

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,4 +1,4 @@
-function validar_nombre_usuario(nombre){
+/* function validar_nombre_usuario(nombre){
     var exp = /[A-Z]([a-z]+)?/; //Expresion solo letras la primera en mayuscula
     return exp.test(nombre);
 }
@@ -15,4 +15,41 @@ function validar_contrasena(contrasena){
 
 module.exports.validar_nombre_usuario = validar_nombre_usuario;
 module.exports.validar_contrasena = validar_contrasena;
-module.exports.validar_anoNacimiento_usuario = validar_anoNacimiento_usuario;
+module.exports.validar_anoNacimiento_usuario = validar_anoNacimiento_usuario; */
+
+let registros = []; 
+
+function obtenerNombre(){
+    var name = document.getElementById("nombre_usuario").value;
+    return name;
+}
+
+function obtenerContraseña(){
+    var password = document.getElementById("contrasena_usuario").value;
+    return password;
+}
+
+function obtenerAño(){
+    var year = document.getElementById("anoNacimiento_usuario").value;
+    return year;
+}
+
+function Usuario(name,password,year){
+    this.name = name;
+    this.password = password;
+    this.year = year;
+}
+
+function agregarRegistro(){
+    var user = new Usuario(obtenerNombre(),obtenerContraseña(),obtenerAño());
+    registros.push(user);
+}
+
+function EncontrarUsuarioPorEdad(arreglo){
+    var edad;
+    for (let index = 0; index < arreglo.length(); index++) {
+        aux_user = arreglo[index];
+        edad = aux_user.year;
+        
+    }
+}
