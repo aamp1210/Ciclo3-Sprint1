acbb95c2b88f287e191a33c8af93c8453a882662
Sebastian Franco <james.franco.velandia@unillanos.edu.co>
2021-10-14 11:55:50 -0500

    Ajustes previos al sprint 4

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,19 +1,28 @@
-/* function validar_nombre_usuario(nombre){
+function validar_nombre_usuario(nombre){
     var exp = /[A-Z]([a-z]+)?/; //Expresion solo letras la primera en mayuscula
+    if (!exp.test(nombre)) {
+        alert('El usuario debe tener la primer letra mayuscula');
+    }
     return exp.test(nombre);
 }
 
 function validar_anoNacimiento_usuario(anio){
     var exp = /(19[0-9][0-9]|20[0-2][0-1])/; //Expresion rango 1900 hasta 2021
+    if (!exp.test(anio)) {
+        alert('El año de nacimiento debe estar entre 1900 - 2021');
+    }
     return exp.test(anio);
 }
 
 function validar_contrasena(contrasena){
     var exp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*)[A-Za-z\d$@$!%*?&]{6,20}$/; //Expresion contraseña
+    if (!exp.test(contrasena)) {
+        alert('La contraseña debe tener entre 6 y 20 caracteres, una minuscula y una mayuscula');
+    }
     return exp.test(contrasena);
 }
 
-module.exports.validar_nombre_usuario = validar_nombre_usuario;
+/* module.exports.validar_nombre_usuario = validar_nombre_usuario;
 module.exports.validar_contrasena = validar_contrasena;
 module.exports.validar_anoNacimiento_usuario = validar_anoNacimiento_usuario; */
 
@@ -40,6 +49,6 @@ function EncontrarUsuarioPorEdad(arreglo){
     return usuariomayor;
 }
 
-module.exports.registros = registros;
+/* module.exports.registros = registros;
 module.exports.EncontrarUsuarioPorEdad = EncontrarUsuarioPorEdad;
-module.exports.agregarRegistro = agregarRegistro;
+module.exports.agregarRegistro = agregarRegistro; */
